e9d9c9eff5d75229da1811f5beb1a99158221d42
boca5tudor <boca_tudor@yahoo.com>
2019-04-07 21:16:32 +0100

    Add project files.

---
+++ TrainStationTest/TestUI.cs
@@ -0,0 +1,69 @@
+﻿namespace TrainStationTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+
+    public partial class TestUI : Form
+    {
+        public TestUI()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nostationlbl.Text = "";
+            liststations.Clear();
+            keylistview.Clear();
+
+            string lookUp = searchTxtBox.Text.ToString();
+            string first = lookUp[0].ToString().ToUpper();
+            List<string> searchStation = new List<string>();
+            List<string> allStations = new TrainStations().getAllTrainStations(first);
+
+            if (allStations.Count() == 0)
+            {
+                nostationlbl.Text = "No such station found";
+            }
+            else
+            {
+                foreach (var station in allStations)
+                {
+                    lookUp = lookUp.First().ToString().ToUpper() + lookUp.Substring(1);
+                    if (station.Contains(lookUp))
+                    {
+                        searchStation.Add(station);
+                        liststations.Items.Add(station);
+                    }
+                    else if (searchStation.Count == 0)
+                    {
+                        nostationlbl.Text = "No station with this name";
+                    }
+                }
+
+                List<char> nextKeysLook = new List<char>();
+                for (int i = 0; i < searchStation.Count(); i++)               
+                {
+                    if (searchStation[i].Length > lookUp.Length)
+                    {
+                        char nextKey;
+                        nextKey = searchStation[i][lookUp.Length + 1];
+                        nextKeysLook.Add(nextKey);
+
+                        if (nextKeysLook.Count() == 1)
+                        {
+                            keylistview.Items.Add(nextKey.ToString().ToUpper());
+                        }
+                        else if(nextKeysLook[i] != nextKeysLook[i-1])
+                        {
+                            keylistview.Items.Add(nextKey.ToString().ToUpper());
+                        }
+                    }
+                }              
+            }
+        }
+    }
+}
---
+++ TrainStationTest/TrainStations.cs
@@ -0,0 +1,57 @@
+﻿namespace TrainStationTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+
+    class TrainStations
+    {
+        // public List<string> AllTrainStations { get; set; }
+
+        //// public method that returns a shortlist of stations based on the first letter typed
+        public List<string> getAllTrainStations(string first)
+        {
+            //char[] list1 = { 'a', 'b', 'c', 'd', 'e' };  //char[] list2 = { 'f', 'g', 'h', 'i', 'j' };
+            //char[] list3 = { 'k', 'l', 'm', 'n', 'o' }; //char[] list4 = { 'p', 'q', 'r', 's', 't' };
+            //char[] list5 = { 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            first = first.ToUpper();
+            List<string> list = new List<string>();           
+            List<string> shortlist = new List<string>();
+
+            // for getting the path to the txt file remotely
+            string pathFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"txtStations\StationsUK.txt");
+            StreamReader file = new StreamReader(pathFolder);
+
+            try
+            {    // create list of stations without ","
+                string station = string.Empty;
+                while ((station = file.ReadLine()) != null)
+                {
+                    list = station.Split(',').ToList();
+                }
+
+                // loop through stations and find the ones that start with the first letter typed
+                for (int i = 0; i <list.Count; i++)
+                {
+                    string stationTyped = list[i];
+                   // strange occurence that needs checking, without the "or" statement this returns one or all-1
+                   // the || returns all the items that match
+                    if (stationTyped[0].ToString().ToUpper().Equals(first) || stationTyped[1].ToString().ToUpper().Equals(first))
+                    {
+                        shortlist.Add(stationTyped);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                shortlist = null;
+                throw;               
+            }
+
+            return shortlist;
+        }
+    }
+}
